11748007de6a996e62f5c8f4da67f11392e0096d
Dan Voell <danvoell@Dans-MacBook-Pro.local>
2013-01-10 16:12:45 -0600

    attempt 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleRailsListingAndProfileDesign::Application.config.secret_token = 'a2332a53dbc426a0702edabd45d99d53ffa3c993c6e950cbc71bf869934cd8228c0093b1a634c92e7da9f9ac45b7701e777381531f1daf6ee88c91f614f59a71'
