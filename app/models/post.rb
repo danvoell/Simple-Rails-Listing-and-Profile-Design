c3e714ec5cc18b17d7de8b9d15430f434c59bf4d
Dan Voell <danvoell@Dans-MacBook-Pro.local>
2013-01-11 12:23:52 -0600

    small change

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ActiveRecord::Base
   attr_accessible :description, :picture, :subheading, :title, :image, :remote_image_url
   mount_uploader :image, ImageUploader
+
+def to_param
+"#{id}-#{title}".parameterize
+end
+
 end
