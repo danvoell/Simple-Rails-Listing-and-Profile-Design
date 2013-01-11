608d60b417dbb161a15543ee7111dc01893adddd
Dan Voell <danvoell@Dans-MacBook-Pro.local>
2013-01-11 07:32:45 -0600

    more css and image size change

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,7 +11,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :thumb do
-    process :resize_to_limit => [200, 200]
+    process :resize_to_limit => [120, 120]
   end
 
 version :medium do
